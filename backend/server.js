abfeb58bc071b22567a09c73585a71036d97bdf4
Supphaset Engphaiboon <supphaset555@gmail.com>
2021-12-03 09:52:57 +0700

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const connectDB = require('./config/db')
 const Table = require('./models/itemModel')
@@ -26,6 +27,20 @@ app.put('/api/subtract/:id', async (req,res)=>{
     const item = await Table.findByIdAndUpdate(req.params.id,{$inc:{qty:-1}})
     res.json(item)
 })
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
